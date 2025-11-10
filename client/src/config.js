8c24aaaaa249bcdd314e5038d4802b49c2ae12c3
Waithera-Macharia-J <joycemacharia074@gmail.com>
2025-11-10 06:51:38 +0300

    Update config.js settings for frontend

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const isLocal = window.location.hostname === 'localhost';
 
 const BACKEND_URL = isLocal
-  ? 'http://localhost:5000'             // Local development
-  : 'http://192.168.49.2:30606';       // Minikube NodePort for backend
+  ? 'http://localhost:5000'
+  : 'http://192.168.49.2:30606'; // NodePort or LoadBalancer IP
 
 export default BACKEND_URL;
